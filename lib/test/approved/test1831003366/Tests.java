869813b49a35da32aaf4756ea0fca201ffd6d71f
kulikov <kulikov@kulikov.office.devexperts.com>
2011-04-28 18:41:24 +0400

    CBR #70

---
+++ lib/test/approved/test1831003366/Tests.java
@@ -0,0 +1,9 @@
+package approved.test1831003366;
+
+import net.egork.utils.test.Test;
+public class Tests {
+	public static final Test[] TESTS = {
+		new Test("on  codeforces \nbeta round is running\n   a rustling of keys ", "YES\n"),
+		new Test("how many gallons\nof edo s rain did you drink\n                                cuckoo", "NO"),
+	};
+}
