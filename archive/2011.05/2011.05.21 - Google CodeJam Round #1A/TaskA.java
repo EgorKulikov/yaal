0ff65d3bc19f6e3013a5ccf60ce70c90e1c3d1e8
Egor <Egor@Egor-??>
2011-05-21 08:16:06 +0400

    GCJ1

---
+++ archive/2011.05/2011.05.21 - Google CodeJam Round #1A/TaskA.java
@@ -0,0 +1,28 @@
+import net.egork.utils.Solver;
+import net.egork.utils.io.InputReader;
+
+import java.io.PrintWriter;
+
+public class TaskA implements Solver {
+	public void solve(int testNumber, InputReader in, PrintWriter out) {
+		long maxGamesToday = in.readLong();
+		int percentToday = in.readInt();
+		int percentTotal = in.readInt();
+		out.print("Case #" + testNumber + ": ");
+		if (percentTotal == 100 || percentTotal == 0) {
+			if (percentToday == percentTotal)
+				out.println("Possible");
+			else
+				out.println("Broken");
+			return;
+		}
+		for (int i = 1; i <= 100 && i <= maxGamesToday; i++) {
+			if (percentToday * i % 100 == 0) {
+				out.println("Possible");
+				return;
+			}
+		}
+		out.println("Broken");
+	}
+}
+
