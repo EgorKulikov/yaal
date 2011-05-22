74d795b16447cb6e776cbb79d894224db6dd8418
Egor <Egor@Egor-??>
2011-05-22 15:23:49 +0400

    IX Programming Olympiads in Murcia

---
+++ archive/2011.05/2011.05.22 - IX Programming Olympiads in Murcia/TaskF.java
@@ -0,0 +1,23 @@
+import net.egork.numbers.IntegerUtils;
+import net.egork.utils.Solver;
+import net.egork.utils.io.InputReader;
+
+import java.io.PrintWriter;
+
+public class TaskF implements Solver {
+	public void solve(int testNumber, InputReader in, PrintWriter out) {
+		int count = in.readInt();
+		long result = 0;
+		long numerator = IntegerUtils.factorial(count);
+		long denominator = 1;
+		for (int i = 0; i <= count; i++) {
+			if (i % 2 == 0)
+				result += numerator / denominator;
+			else
+				result -= numerator / denominator;
+			denominator *= i + 1;
+		}
+		out.println(result + "/" + numerator);
+	}
+}
+
---
+++ lib/test/approved/test1479782644/Tests.java
@@ -0,0 +1,8 @@
+package approved.test1479782644;
+
+import net.egork.utils.test.Test;
+public class Tests {
+	public static final Test[] TESTS = {
+		new Test("8\n1 6\n2 28\n4 5\n5 26\n8 1\n11 1\n12 25\n12 31", "Thursday\nMonday\nTuesday\nThursday\nMonday\nTuesday\nSunday\nSaturday"),
+	};
+}
