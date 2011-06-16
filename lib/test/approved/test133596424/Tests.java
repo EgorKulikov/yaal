df1355e021fc7ae8201737ce02030ea6e2c39a97
Egor <Egor@Egor-??>
2011-06-16 21:33:16 +0400

    cbr74 ccjune

---
+++ lib/test/approved/test133596424/Tests.java
@@ -0,0 +1,9 @@
+package approved.test133596424;
+
+import net.egork.utils.test.Test;
+public class Tests {
+	public static final Test[] TESTS = {
+		new Test("3\n1 0 1", "ARARARALLLA"),
+		new Test("3\n0 2 0", "ARARALAARALA"),
+	};
+}
