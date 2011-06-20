54cf300efe7c7265d27f849d205b103e2e5bc292
kulikov <kulikov@kulikov.office.devexperts.com>
2011-06-20 21:43:11 +0400

    cbr75

---
+++ lib/test/approved/test1421529331/Tests.java
@@ -0,0 +1,8 @@
+package approved.test1421529331;
+
+import net.egork.utils.test.Test;
+public class Tests {
+	public static final Test[] TESTS = {
+		new Test("3 4\n1 3\n2 3\n1 2\n1 2", "0\n0\n1\n3"),
+	};
+}
