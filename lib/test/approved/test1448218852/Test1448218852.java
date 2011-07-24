1a307557e0bbfabec5e445f69153b45550f4bc3b
Egor <Egor@Egor-??>
2011-07-24 22:44:53 +0400

    july cook-off

---
+++ lib/test/approved/test1448218852/Test1448218852.java
@@ -0,0 +1,47 @@
+package approved.test1448218852;
+
+import net.egork.utils.io.stringinputreader.StringInputReader;
+import net.egork.utils.test.Test;
+import org.junit.Assert;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Locale;
+
+public class Test1448218852 {
+	@org.junit.Test
+	public void test() throws Exception {
+		Locale.setDefault(Locale.US);
+		Collection<Test> tests = Arrays.asList(Tests.TESTS);
+		tests.addAll(MainChecker.generateTests());
+		for (Test test : tests) {
+			StringWriter output = new StringWriter();
+			try {
+				Main.run(new StringInputReader(test.getInput()), new PrintWriter(output));
+				String verdict = MainChecker.check(new StringInputReader(test.getInput()),
+					new StringInputReader(test.getExpectedOutput()), new StringInputReader(output.toString()));
+				if (verdict != null) {
+					System.err.println("Failed with wrong answer: " + verdict);
+					System.err.println("Input:");
+					System.err.println(test.getInput());
+					System.err.println("Expected output:");
+					System.err.println(test.getExpectedOutput());
+					System.err.println("Actual output:");
+					System.err.println(output.toString());
+					Assert.fail();
+				}
+			} catch (Throwable e) {
+				System.err.println("Failed with runtime error");
+				System.err.println("Input:");
+				System.err.println(test.getInput());
+				System.err.println("Expected output:");
+				System.err.println(test.getExpectedOutput());
+				System.err.println("Exception:");
+				e.printStackTrace();
+				Assert.fail();
+			}
+		}
+	}
+}
