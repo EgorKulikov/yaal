8e7ebe71e19f4c8f51d7bf1fa2606b0161bed657
Egor <Egor@Egor-??>
2011-08-07 19:13:46 +0400

    cbr80

---
+++ lib/test/approved/test690958244/Tests.java
@@ -0,0 +1,9 @@
+package approved.test690958244;
+
+import net.egork.utils.test.Test;
+public class Tests {
+	public static final Test[] TESTS = {
+		new Test("6 6\n6 3\n6 4\n5 1\n2 5\n1 4\n5 4", "FHTAGN!\n"),
+		new Test("6 5\n5 6\n4 6\n3 1\n5 1\n1 2", "NO"),
+	};
+}
