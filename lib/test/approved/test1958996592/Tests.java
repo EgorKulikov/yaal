3791f0a651d866a15682d0ca846054545e09c49b
kulikov <kulikov@kulikov.office.devexperts.com>
2011-09-16 15:12:30 +0400

    Codeforces Beta Round #87

---
+++ lib/test/approved/test1958996592/Tests.java
@@ -0,0 +1,10 @@
+package approved.test1958996592;
+
+import net.egork.utils.test.Test;
+public class Tests {
+	public static final Test[] TESTS = {
+		new Test("7 4\n3\n2\n3\n2\n1\n2\n3\n1 2 5\n2 3 5\n3 5 3\n7 7 5", "4"),
+		new Test("2 1\n0\n3\n1 2 5", "2"),
+		new Test("3 1\n10\n10\n10\n1 3 10", "0"),
+	};
+}
