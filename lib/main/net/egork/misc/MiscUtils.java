bd77258976e20356b592b4c122100c2a7adf51ba
Egor <Egor@Egor-??>
2011-10-11 18:40:48 +0400

    Codeforces Beta Round #89

--- lib/main/net/egork/misc/MiscUtils.java
+++ lib/main/net/egork/misc/MiscUtils.java
@@ -130,4 +130,16 @@ public class MiscUtils {
 		int dy = y1 - y2;
 		return dx * dx + dy * dy;
 	}
+
+	public static<T extends Comparable<T>> T min(T first, T second) {
+		if (first.compareTo(second) <= 0)
+			return first;
+		return second;
+	}
+
+	public static<T extends Comparable<T>> T max(T first, T second) {
+		if (first.compareTo(second) <= 0)
+			return second;
+		return first;
+	}
 }
