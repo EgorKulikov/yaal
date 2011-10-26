da7d2d68594790ff80bb89c062adf40fac419615
Egor <Egor@Egor-??>
2011-10-26 07:19:17 +0400

    TopCoder SRM #522

--- lib/main/net/egork/numbers/Rational.java
+++ lib/main/net/egork/numbers/Rational.java
@@ -4,10 +4,13 @@ package net.egork.numbers;
  * @author Egor Kulikov (kulikov@devexperts.com)
  */
 public class Rational implements Comparable<Rational> {
-	private final long numerator;
-	private final long denominator;
 	public static final Rational MAX_VALUE = new Rational(Integer.MAX_VALUE, 1);
-	public static final Rational MIN_VALUE = new Rational(Integer.MIN_VALUE, 1);;
+	public static final Rational MIN_VALUE = new Rational(Integer.MIN_VALUE, 1);
+	public static final Rational ONE = new Rational(1, 1);
+	public static final Rational ZERO = new Rational(0, 1);
+
+	public final long numerator;
+	public final long denominator;
 
 	public Rational(long numerator, long denominator) {
 		if (denominator == 0)
@@ -22,14 +25,6 @@ public class Rational implements Comparable<Rational> {
 		}
 	}
 
-	public long getNumerator() {
-		return numerator;
-	}
-
-	public long getDenominator() {
-		return denominator;
-	}
-
 	@Override
 	public String toString() {
 		return numerator + "/" + denominator;
@@ -37,5 +32,25 @@ public class Rational implements Comparable<Rational> {
 
 	public int compareTo(Rational other) {
 		return IntegerUtils.longCompare(numerator * other.denominator, denominator * other.numerator);
+	}
+
+	public Rational add(Rational other) {
+		return new Rational(numerator * other.denominator + denominator * other.numerator,
+			denominator * other.denominator);
+	}
+
+	public Rational reverse() {
+		if (numerator == 0)
+			throw new ArithmeticException();
+		return new Rational(denominator, numerator);
+	}
+
+	public Rational multiply(long number) {
+		return new Rational(numerator * number, denominator);
+	}
+
+	public Rational subtract(Rational other) {
+		return new Rational(numerator * other.denominator - denominator * other.numerator,
+			denominator * other.denominator);
 	}
 }
