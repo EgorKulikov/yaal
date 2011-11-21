fd4a30214ccb55d53c036debbdda6825dede64b6
kulikov <kulikov@kulikov.office.devexperts.com>
2011-11-21 11:57:12 +0400

    USACO November Gold Contest E-Olimp Weekly #10

---
+++ lib/test/approved/on2011_10_18/taskb/Main.java
@@ -0,0 +1,19 @@
+package approved.on2011_10_18.taskb;
+
+import net.egork.chelper.tester.Tester;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!Tester.test("net.egork.utils.io.InputReader",
+			"approved.on2011_10_18.taskb.TaskB",
+			"SINGLE",
+			"4/__3 2 1 6;;3;;true::4/__1 2 3 4;;-1;;true",
+			"net.egork.utils.io.OutputWriter"))
+		{
+			Assert.fail();
+		}
+	}
+}
