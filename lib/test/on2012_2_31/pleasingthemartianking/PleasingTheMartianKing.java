9ae308b4e33d43f4b5aab42f75854a59c0ae4e78
Egor <Egor@Egor-??>
2012-03-31 18:19:18 +0400

    Paradigm Shift

---
+++ lib/test/on2012_2_31/pleasingthemartianking/PleasingTheMartianKing.java
@@ -0,0 +1,21 @@
+package on2012_2_31.pleasingthemartianking;
+
+
+
+import net.egork.io.IOUtils;
+import net.egork.utils.io.InputReader;
+import net.egork.utils.io.OutputWriter;
+
+public class PleasingTheMartianKing {
+	public void solve(int testNumber, InputReader in, OutputWriter out) {
+		int count = in.readInt();
+		int ones = in.readInt();
+		int[] array = IOUtils.readIntArray(in, count);
+		int answer = 0;
+		for (int i = 0; i < count - ones; i++)
+			answer -= array[i];
+		for (int i = count - ones; i < count; i++)
+			answer += array[i];
+		out.printLine(answer);
+	}
+}
