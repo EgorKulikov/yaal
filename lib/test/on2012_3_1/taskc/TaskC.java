bde41b6abb03d11fccf11e5828a156621bb7760c
Egor <Egor@Egor-??>
2012-04-04 12:57:30 +0400

    April Fools Day Contest

---
+++ lib/test/on2012_3_1/taskc/TaskC.java
@@ -0,0 +1,18 @@
+package on2012_3_1.taskc;
+
+
+
+import net.egork.io.IOUtils;
+import net.egork.utils.io.InputReader;
+import net.egork.utils.io.OutputWriter;
+
+public class TaskC {
+	public void solve(int testNumber, InputReader in, OutputWriter out) {
+		int count = in.readInt();
+		int[] array = IOUtils.readIntArray(in, count);
+		int answer = 0;
+		for (int i = 0; i < count; i++)
+			answer += (i + 1) * array[i];
+		out.printLine(answer);
+	}
+}
