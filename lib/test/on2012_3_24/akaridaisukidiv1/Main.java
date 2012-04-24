d14997ede88de697610b07e606ef215aae57e7b2
kulikov <kulikov@kulikov.office.devexperts.com>
2012-04-24 13:34:26 +0400

    TopCoder SRM #541

---
+++ lib/test/on2012_3_24/akaridaisukidiv1/Main.java
@@ -0,0 +1,17 @@
+package on2012_3_24.akaridaisukidiv1;
+
+import net.egork.chelper.tester.TopCoderTester;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!TopCoderTester.test("int countF(String Waai, String Akari, String Daisuki, String S, String F, int k)",
+			"on2012_3_24.akaridaisukidiv1.AkariDaisukiDiv1",
+			"a;;b;;c;;x;;axb;;2;;2;;true::a;;b;;c;;x;;abcdefghij;;1;;0;;true::a;;a;;a;;b;;aba;;2;;4;;true::a;;b;;c;;d;;baadbdcbadbdccccbaaaadbdcbadbdccbaadbdcba;;58;;191690599;;true::a;;x;;y;;b;;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab;;49;;1;;true::waai;;akari;;daisuki;;usushio;;id;;10000000;;127859200;;true::vfsebgjfyfgerkqlr;;ezbiwls;;kjerx;;jbmjvaawoxycfndukrjfg;;bgjfyfgerkqlrvfsebgjfyfgerkqlrvfsebgjfyfgerkqlrvfs;;1575724;;483599313;;true"))
+		{
+			Assert.fail();
+		}
+	}
+}
