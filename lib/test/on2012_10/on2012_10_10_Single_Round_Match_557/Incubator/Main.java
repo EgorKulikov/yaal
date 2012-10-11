8baf99f915f9bdc0f3438545523c04a7cbb7d095
Egor <Egor@Egor-HP>
2012-10-11 15:11:21 +0400

    TopCoder SRM #557

---
+++ lib/test/on2012_10/on2012_10_10_Single_Round_Match_557/Incubator/Main.java
@@ -0,0 +1,14 @@
+package on2012_10.on2012_10_10_Single_Round_Match_557.Incubator;
+
+import net.egork.chelper.tester.NewTopCoderTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTopCoderTester.test("lib/test/on2012_10/on2012_10_10_Single_Round_Match_557/Incubator/Incubator.tctask"))
+			Assert.fail();
+	}
+}
