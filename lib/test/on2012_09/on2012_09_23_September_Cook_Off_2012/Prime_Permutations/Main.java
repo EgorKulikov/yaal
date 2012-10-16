c37d88d24d7223c1e4099547cf1d17c14ef9dab3
Egor <Egor@Egor-??>
2012-10-16 10:11:38 +0400

    September Cook-Off

---
+++ lib/test/on2012_09/on2012_09_23_September_Cook_Off_2012/Prime_Permutations/Main.java
@@ -0,0 +1,14 @@
+package on2012_09.on2012_09_23_September_Cook_Off_2012.Prime_Permutations;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("lib/test/on2012_09/on2012_09_23_September_Cook_Off_2012/Prime_Permutations/Prime Permutations.task"))
+			Assert.fail();
+	}
+}
