ba4500ebe1d88d798406265c2226342e5239d437
egorku <egorku@EGORKU-W7.ld.yandex.ru>
2012-11-26 20:04:02 +0400

    CROC-MBTU Final Round

---
+++ lib/test/on2012_11/on2012_11_26_CROC_MBTU_Final_Round/C___Movie_Critics/TaskC.java
@@ -0,0 +1,39 @@
+package on2012_11.on2012_11_26_CROC_MBTU_Final_Round.C___Movie_Critics;
+
+
+
+import net.egork.collections.map.Counter;
+import net.egork.io.IOUtils;
+import net.egork.utils.io.InputReader;
+import net.egork.utils.io.OutputWriter;
+
+public class TaskC {
+	public void solve(int testNumber, InputReader in, OutputWriter out) {
+		int count = in.readInt();
+		int genreCount = in.readInt();
+		int[] genres = IOUtils.readIntArray(in, count);
+		Counter<Integer> counter = new Counter<Integer>();
+		counter.add(genres[0]);
+		for (int i = 1; i < count; i++) {
+			if (genres[i] != genres[i - 1]) {
+				counter.add(genres[i]);
+				for (int j = i; j < count; j++) {
+					if (genres[i] != genres[j]) {
+						if (genres[j] == genres[i - 1])
+							counter.add(genres[i]);
+						break;
+					}
+				}
+			}
+		}
+		long delta = -1;
+		int answer = 0;
+		for (int i = 1; i <= genreCount; i++) {
+			if (counter.get(i) > delta) {
+				delta = counter.get(i);
+				answer = i;
+			}
+		}
+		out.printLine(answer);
+	}
+}
---
+++ lib/test/on2012_11/on2012_11_26_CROC_MBTU_Final_Round/D___Building_Bridge/Main.java
@@ -0,0 +1,14 @@
+package on2012_11.on2012_11_26_CROC_MBTU_Final_Round.D___Building_Bridge;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("lib/test/on2012_11/on2012_11_26_CROC_MBTU_Final_Round/D___Building_Bridge/D - Building Bridge.task"))
+			Assert.fail();
+	}
+}
