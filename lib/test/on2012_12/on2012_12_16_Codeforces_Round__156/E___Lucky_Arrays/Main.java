2f0d1059a798bbcc4cd86a48d41bf3474263fce0
User <User@Desktop>
2012-12-22 13:55:21 +0400

    Codeforces Round #156

---
+++ lib/test/on2012_12/on2012_12_16_Codeforces_Round__156/E___Lucky_Arrays/Main.java
@@ -0,0 +1,14 @@
+package on2012_12.on2012_12_16_Codeforces_Round__156.E___Lucky_Arrays;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("lib/test/on2012_12/on2012_12_16_Codeforces_Round__156/E___Lucky_Arrays/E - Lucky Arrays.task"))
+			Assert.fail();
+	}
+}
