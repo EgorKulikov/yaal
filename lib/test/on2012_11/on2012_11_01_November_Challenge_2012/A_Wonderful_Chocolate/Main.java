e093d82e869ba83d8b43a1cf2fbf713fa4227887
User <User@Desktop>
2012-12-23 14:51:19 +0400

    Missing files

---
+++ lib/test/on2012_11/on2012_11_01_November_Challenge_2012/A_Wonderful_Chocolate/Main.java
@@ -0,0 +1,14 @@
+package on2012_11.on2012_11_01_November_Challenge_2012.A_Wonderful_Chocolate;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("lib/test/on2012_11/on2012_11_01_November_Challenge_2012/A_Wonderful_Chocolate/A Wonderful Chocolate.task"))
+			Assert.fail();
+	}
+}
