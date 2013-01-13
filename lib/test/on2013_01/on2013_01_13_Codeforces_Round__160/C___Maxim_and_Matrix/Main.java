ef6d1fa99cac9d14137307d8a419bb3607c62f33
User <User@Desktop>
2013-01-13 21:53:36 +0400

    Codeforces Round #160

---
+++ lib/test/on2013_01/on2013_01_13_Codeforces_Round__160/C___Maxim_and_Matrix/Main.java
@@ -0,0 +1,14 @@
+package on2013_01.on2013_01_13_Codeforces_Round__160.C___Maxim_and_Matrix;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("lib/test/on2013_01/on2013_01_13_Codeforces_Round__160/C___Maxim_and_Matrix/C - Maxim and Matrix.task"))
+			Assert.fail();
+	}
+}
