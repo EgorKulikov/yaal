ca05139f8d4d2ae7a69eb640d120a87532feba48
egorku <egorku@egorku-nb-w7.ld.yandex.ru>
2013-01-22 16:51:02 +0400

    Suffix Automaton

---
+++ lib/test/on2013_01/on2013_01_22_Codeforces_Beta_Round__92__Div__1_Only_/D___String/Main.java
@@ -0,0 +1,14 @@
+package on2013_01.on2013_01_22_Codeforces_Beta_Round__92__Div__1_Only_.D___String;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("lib/test/on2013_01/on2013_01_22_Codeforces_Beta_Round__92__Div__1_Only_/D___String/D - String.task"))
+			Assert.fail();
+	}
+}
