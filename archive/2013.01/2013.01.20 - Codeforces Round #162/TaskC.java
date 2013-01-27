56a8c707817887fa25f638da9bc01ad80ece587b
User <User@Desktop>
2013-01-27 22:17:33 +0400

    Codeforces Round #162 January Cook-Off 2013

---
+++ archive/2013.01/2013.01.20 - Codeforces Round #162/TaskC.java
@@ -0,0 +1,44 @@
+package net.egork;
+
+import net.egork.io.IOUtils;
+import net.egork.misc.MiscUtils;
+import net.egork.utils.io.InputReader;
+import net.egork.utils.io.OutputWriter;
+
+import java.util.Arrays;
+
+public class TaskC {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+		int count = in.readInt();
+		int queryCount = in.readInt();
+		int[] values = IOUtils.readIntArray(in, count);
+		int[] colors = IOUtils.readIntArray(in, count);
+		MiscUtils.decreaseByOne(colors);
+		long[] result = new long[count];
+		for (int i = 0; i < queryCount; i++) {
+			Arrays.fill(result, Long.MIN_VALUE / 2);
+			int maxIndex = 0;
+			long max = 0;
+			long secondMax = 0;
+			long same = in.readInt();
+			long other = in.readInt();
+			for (int j = 0; j < count; j++) {
+				long current;
+				if (maxIndex == colors[j]) {
+					current = Math.max(same * values[j] + result[colors[j]], other * values[j] + secondMax);
+					max = Math.max(max, current);
+				} else {
+					current = Math.max(same * values[j] + result[colors[j]], other * values[j] + max);
+					if (current > max) {
+						secondMax = max;
+						max = current;
+						maxIndex = colors[j];
+					} else
+						secondMax = Math.max(secondMax, current);
+				}
+				result[colors[j]] = Math.max(result[colors[j]], current);
+			}
+			out.printLine(max);
+		}
+	}
+}
