60d9dfe6e631d6cb67678d17800e7463e684827d
Egor <Egor@egor-laptop>
2013-05-11 22:23:17 +0400

    TopCoder Open Round #2C

---
+++ archive/2013.05/2013.05.11 - TopCoder Open Round #2C/DancingFoxes.java
@@ -0,0 +1,30 @@
+package net.egork;
+
+import net.egork.collections.Pair;
+import net.egork.collections.intcollection.IntList;
+import net.egork.graph.BidirectionalGraph;
+import net.egork.graph.Graph;
+import net.egork.graph.ShortestDistance;
+
+public class DancingFoxes {
+    public int minimalDays(String[] friendship) {
+		int count = friendship.length;
+		Graph graph = new BidirectionalGraph(count);
+		for (int i = 0; i < count; i++) {
+			for (int j = i + 1; j < count; j++) {
+				if (friendship[i].charAt(j) == 'Y')
+					graph.addWeightedEdge(i, j, 1);
+			}
+		}
+		Pair<Long,IntList> result = ShortestDistance.dijkstraAlgorithm(graph, 0, 1);
+		if (result == null)
+			return -1;
+		long length = result.first + 1;
+		int answer = 0;
+		while (length != 2) {
+			length -= length / 3;
+			answer++;
+		}
+		return answer;
+    }
+}
