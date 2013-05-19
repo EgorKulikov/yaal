2a3354ca0f87f46dba9358bc0f1d7c0bb982c0c9
Egor <Egor@egor-laptop>
2013-05-19 19:55:16 +0400

    TopCoder SRM #579

---
+++ lib/test/on2013_05/on2013_05_18_Single_Round_Match_579/UndoHistory/UndoHistory.java
@@ -0,0 +1,30 @@
+package on2013_05.on2013_05_18_Single_Round_Match_579.UndoHistory;
+
+
+
+import net.egork.collections.set.EHashSet;
+
+import java.util.Set;
+
+public class UndoHistory {
+    public int minPresses(String[] lines) {
+		Set<String> undo = new EHashSet<String>();
+		undo.add("");
+		int answer = 0;
+		String last = "";
+		for (String line : lines) {
+			int current = Integer.MAX_VALUE;
+			if (line.startsWith(last))
+				current = line.length() - last.length() + 1;
+			for (String s : undo) {
+				if (line.startsWith(s))
+					current = Math.min(current, line.length() - s.length() + 3);
+			}
+			answer += current;
+			for (int i = 1; i <= line.length(); i++)
+				undo.add(line.substring(0, i));
+			last = line;
+		}
+		return answer;
+    }
+}
