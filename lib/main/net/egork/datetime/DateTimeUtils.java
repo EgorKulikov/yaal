cdcfe6fbe677b1b62fc8ccb1a96769f2c836b3f5
egorku <egorku@egorku-nb-w7.ld.yandex.ru>
2013-05-30 17:30:47 +0400

    Library

--- lib/main/net/egork/datetime/DateTimeUtils.java
+++ lib/main/net/egork/datetime/DateTimeUtils.java
@@ -4,7 +4,7 @@ package net.egork.datetime;
  * @author Egor Kulikov (kulikov@devexperts.com)
  */
 public class DateTimeUtils {
-	public static Time minimalTimePassed(Time from, Time to) {
+	public static Time timePassed(Time from, Time to) {
 		int hours = to.getHours() - from.getHours();
 		int minutes = to.getMinutes() - from.getMinutes();
 		int seconds = to.getSeconds() - from.getSeconds();
