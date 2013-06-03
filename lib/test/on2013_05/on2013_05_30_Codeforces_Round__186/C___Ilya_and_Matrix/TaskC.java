68188378ea6f9bb46e6c3d251fc14402e8ffa678
egorku <egorku@egorku-nb-w7.ld.yandex.ru>
2013-06-03 12:01:48 +0400

    Codeforces Round #186

---
+++ lib/test/on2013_05/on2013_05_30_Codeforces_Round__186/C___Ilya_and_Matrix/TaskC.java
@@ -0,0 +1,24 @@
+package on2013_05.on2013_05_30_Codeforces_Round__186.C___Ilya_and_Matrix;
+
+
+
+import net.egork.collections.comparators.IntComparator;
+import net.egork.io.IOUtils;
+import net.egork.misc.ArrayUtils;
+import net.egork.utils.io.InputReader;
+import net.egork.utils.io.OutputWriter;
+
+public class TaskC {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+		int count = in.readInt();
+		int[] numbers = IOUtils.readIntArray(in, count);
+		ArrayUtils.sort(numbers, IntComparator.REVERSE);
+		long[] sum = new long[count + 1];
+		for (int i = 0; i < count; i++)
+			sum[i + 1] = sum[i] + numbers[i];
+		long answer = 0;
+		for (int size = 1; size <= count; size *= 4)
+			answer += sum[size];
+		out.printLine(answer);
+    }
+}
