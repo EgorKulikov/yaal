66f87a1e60962cf1385f40aa4a23e483247edf95
Egor <Egor@egor-laptop>
2013-06-04 13:22:44 +0400

    TopCoder SRM #581

---
+++ lib/test/on2013_06/on2013_06_04_Single_Round_Match_581/TreeUnion/Main.java
@@ -0,0 +1,14 @@
+package on2013_06.on2013_06_04_Single_Round_Match_581.TreeUnion;
+
+import net.egork.chelper.tester.NewTopCoderTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTopCoderTester.test("lib/test/on2013_06/on2013_06_04_Single_Round_Match_581/TreeUnion/TreeUnion.tctask"))
+			Assert.fail();
+	}
+}
