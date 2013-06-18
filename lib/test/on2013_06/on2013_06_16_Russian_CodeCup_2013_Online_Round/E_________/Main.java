f1479f6d8fcf3027e4259ce075fc7406e00fecd4
egorku <egorku@egorku-nb-w7.ld.yandex.ru>
2013-06-18 16:21:51 +0400

    Unit test fixes

--- lib/test/on2013_06/on2013_06_16_Russian_CodeCup_2013_Online_Round/E_________/Main.java
+++ lib/test/on2013_06/on2013_06_16_Russian_CodeCup_2013_Online_Round/E_________/Main.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class Main {
 	@Test
 	public void test() throws Exception {
-		if (!NewTester.test("lib/test/on2013_06/on2013_06_16_Russian_CodeCup_2013_Online_Round/E_________/E - Лазеры.task"))
+		if (!NewTester.test("lib/test/on2013_06/on2013_06_16_Russian_CodeCup_2013_Online_Round/E_________/E.task"))
 			Assert.fail();
 	}
 }
