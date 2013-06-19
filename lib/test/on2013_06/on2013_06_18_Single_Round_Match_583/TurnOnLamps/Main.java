3d6f4ccf2dd88be226f82216ae3837c4884859a9
egorku <egorku@egorku-nb-w7.ld.yandex.ru>
2013-06-19 16:13:20 +0400

    TopCoder SRM #583

---
+++ lib/test/on2013_06/on2013_06_18_Single_Round_Match_583/TurnOnLamps/Main.java
@@ -0,0 +1,14 @@
+package on2013_06.on2013_06_18_Single_Round_Match_583.TurnOnLamps;
+
+import net.egork.chelper.tester.NewTopCoderTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTopCoderTester.test("lib/test/on2013_06/on2013_06_18_Single_Round_Match_583/TurnOnLamps/TurnOnLamps.tctask"))
+			Assert.fail();
+	}
+}
