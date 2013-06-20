f445cd9310a4dbda8839e3f4efc53be4c7e9af31
egorku <egorku@egorku-nb-w7.ld.yandex.ru>
2013-06-20 12:40:46 +0400

    Geometry

---
+++ lib/test/on2013_06/on2013_06_19_9_Round_Spiral__Round_3__Step_10_/F__Affiliation_point_segment/Main.java
@@ -0,0 +1,14 @@
+package on2013_06.on2013_06_19_9_Round_Spiral__Round_3__Step_10_.F__Affiliation_point_segment;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("lib/test/on2013_06/on2013_06_19_9_Round_Spiral__Round_3__Step_10_/F__Affiliation_point_segment/F. Affiliation point segment.task"))
+			Assert.fail();
+	}
+}
