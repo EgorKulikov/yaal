685ce0f389f3adfefb2c499fa2cb93afef629f95
Egor <Egor@egor-laptop>
2013-09-27 14:03:07 +0400

    Unit test fix

--- lib/test/on2013_09/on2013_09_23_Russian_CodeCup_2013_Final_Round/C________________________________/Main.java
+++ lib/test/on2013_09/on2013_09_23_Russian_CodeCup_2013_Final_Round/C________________________________/Main.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class Main {
 	@Test
 	public void test() throws Exception {
-		if (!NewTester.test("lib/test/on2013_09/on2013_09_23_Russian_CodeCup_2013_Final_Round/C________________________________/C - Быстрая-пребыстрая сортировка.task"))
+		if (!NewTester.test("lib/test/on2013_09/on2013_09_23_Russian_CodeCup_2013_Final_Round/C________________________________/C.task"))
 			Assert.fail();
 	}
 }
--- lib/test/on2013_09/on2013_09_23_Russian_CodeCup_2013_Final_Round/D________/Main.java
+++ lib/test/on2013_09/on2013_09_23_Russian_CodeCup_2013_Final_Round/D________/Main.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class Main {
 	@Test
 	public void test() throws Exception {
-		if (!NewTester.test("lib/test/on2013_09/on2013_09_23_Russian_CodeCup_2013_Final_Round/D________/D - Робот.task"))
+		if (!NewTester.test("lib/test/on2013_09/on2013_09_23_Russian_CodeCup_2013_Final_Round/D________/D.task"))
 			Assert.fail();
 	}
 }
