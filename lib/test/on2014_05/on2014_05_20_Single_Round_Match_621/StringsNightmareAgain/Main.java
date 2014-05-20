9d48b36a056ead04604f96f38ac8067994c5c1fe
egorku <egorku@egorku-n-w7.ld.yandex.ru>
2014-05-20 21:36:39 +0400

    TopCoder SRM #621

---
+++ lib/test/on2014_05/on2014_05_20_Single_Round_Match_621/StringsNightmareAgain/Main.java
@@ -0,0 +1,14 @@
+package on2014_05.on2014_05_20_Single_Round_Match_621.StringsNightmareAgain;
+
+import net.egork.chelper.tester.NewTopCoderTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTopCoderTester.test("lib/test/on2014_05/on2014_05_20_Single_Round_Match_621/StringsNightmareAgain/StringsNightmareAgain.tctask"))
+			Assert.fail();
+	}
+}
