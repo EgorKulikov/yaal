5522dd252d571306010cdc3e9fdf3bb97a83f0c7
Egor Kulikov <egor@egork.net>
2018-06-15 18:03:47 +0200

    TopCoder Open Round #2B

---
+++ archive/2018.06/2018.06.14 - TopCoder Open Round #2B/SquareFreeSet.java
@@ -0,0 +1,49 @@
+package net.egork;
+
+import net.egork.generated.collections.list.IntArrayList;
+import net.egork.generated.collections.list.IntList;
+import net.egork.generated.collections.set.IntHashSet;
+import net.egork.graph.Graph;
+import net.egork.graph.MinCostFlow;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.max;
+import static java.util.Arrays.binarySearch;
+
+public class SquareFreeSet {
+    private static final int BUBEN = 70;
+
+    public int findCost(int[] x) {
+        IntHashSet candidates = new IntHashSet();
+        for (int i : x) {
+            for (int j = max(i - x.length - BUBEN, 1); j <= i + x.length + BUBEN; j++) {
+                candidates.add(squareFree(j));
+            }
+        }
+        IntList list = new IntArrayList(candidates);
+        list.sort();
+        int[] arr = list.toArray();
+        Graph graph = new Graph(x.length + list.size() + 2);
+        int source = x.length + list.size();
+        int sink = source + 1;
+        for (int i = 0; i < x.length; i++) {
+            graph.addFlowWeightedEdge(source, i, 0, 1);
+            for (int j = max(x[i] - x.length - BUBEN, 1); j <= x[i] + x.length + BUBEN; j++) {
+                graph.addFlowWeightedEdge(i, x.length + binarySearch(arr, squareFree(j)), abs(j - x[i]), 1);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            graph.addFlowWeightedEdge(x.length + i, sink, 0, 1);
+        }
+        return (int)(long)MinCostFlow.minCostMaxFlow(graph, source, sink, false).first;
+    }
+
+    private int squareFree(int j) {
+        for (int i = 2; i * i <= j; i++) {
+            while (j % (i * i) == 0) {
+                j /= i * i;
+            }
+        }
+        return j;
+    }
+}
---
+++ archive/2018.06/2018.06.14 - TopCoder Open Round #2B/SubarrayAverages.java
@@ -0,0 +1,26 @@
+package net.egork;
+
+public class SubarrayAverages {
+    public double[] findBest(int[] arr) {
+        double[] result = new double[arr.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = arr[i];
+        }
+        for (int i = 0; i < result.length; i++) {
+            double best = result[i];
+            int to = i;
+            double sum = result[i];
+            for (int j = i + 1; j < result.length; j++) {
+                sum += result[j];
+                if (sum / (j - i + 1) < best) {
+                    best = sum / (j - i + 1);
+                    to = j;
+                }
+            }
+            for (int j = i; j <= to; j++) {
+                result[j] = best;
+            }
+        }
+        return result;
+    }
+}
