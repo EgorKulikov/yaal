ba9fb136b5c12999147e5e45284e7275415afab7
Egor Kulikov <egor@egork.net>
2018-07-19 01:48:37 +0200

    CSAcademy Round #84

---
+++ lib/test/on2018_07/on2018_07_19_Round_84/Growing_Trees/GrowingTrees.java
@@ -0,0 +1,69 @@
+package on2018_07.on2018_07_19_Round_84.Growing_Trees;
+
+
+
+import net.egork.graph.BidirectionalGraph;
+import net.egork.graph.Graph;
+import net.egork.io.InputReader;
+import net.egork.io.OutputWriter;
+
+import static net.egork.misc.MiscUtils.decreaseByOne;
+
+public class GrowingTrees {
+    Graph graph;
+    int[] c;
+    int[] a;
+
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.readInt();
+        int d = in.readInt();
+        int[] s = new int[n - 1];
+        int[] e = new int[n - 1];
+        c = new int[n - 1];
+        a = new int[n - 1];
+        in.readIntArrays(s, e, c, a);
+        decreaseByOne(s, e);
+        graph = BidirectionalGraph.createGraph(n, s, e);
+        int left = 0;
+        int right = d;
+        while (left < right) {
+            int middle = (left + right) >> 1;
+            long delta = calculate(middle + 1) - calculate(middle);
+            if (delta >= 0) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        out.printLine(left);
+        out.printLine(calculate(left));
+    }
+
+    long diameter;
+
+    private long calculate(int d) {
+        diameter = 0;
+        go(0, -1, d);
+        return diameter;
+    }
+
+    private long go(int vertex, int last, int d) {
+        long best = 0;
+        long second = 0;
+        for (int i = graph.firstOutbound(vertex); i != -1; i = graph.nextOutbound(i)) {
+            int to = graph.destination(i);
+            if (to == last) {
+                continue;
+            }
+            long call = go(to, vertex, d) + c[i >> 1] + (long)d * a[i >> 1];
+            if (call > best) {
+                second = best;
+                best = call;
+            } else {
+                second = Math.max(second, call);
+            }
+        }
+        diameter = Math.max(diameter, best + second);
+        return best;
+    }
+}
---
+++ lib/test/on2018_07/on2018_07_19_Round_84/Growing_Trees/Main.java
@@ -0,0 +1,14 @@
+package on2018_07.on2018_07_19_Round_84.Growing_Trees;
+
+import net.egork.chelper.tester.NewTester;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main {
+	@Test
+	public void test() throws Exception {
+		if (!NewTester.test("lib/test/on2018_07/on2018_07_19_Round_84/Growing_Trees/Growing Trees.json"))
+			Assert.fail();
+	}
+}
